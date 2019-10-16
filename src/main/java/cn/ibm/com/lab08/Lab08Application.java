496a54ef5ae175c008d5cff676e8f49347274a46
dkdingdk <dkdingdk@cn.ibm.com>
2019-10-16 16:22:08 +0800

    20191016-add the maven assignment configuration

--- src/main/java/cn/ibm/com/lab08/Lab08Application.java
+++ src/main/java/cn/ibm/com/lab08/Lab08Application.java
@@ -2,12 +2,20 @@ package cn.ibm.com.lab08;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Lab08Application {
+public class Lab08Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Lab08Application.class, args);
 	}
 
+	// For support local tomcat
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		builder.sources(this.getClass());
+		return super.configure(builder);
+	}
 }
